d5fb37f5b73d27dce32c62933a71c20b56b7f467
Danny Reagan <t_d_regan@yahoo.com>
2018-04-14 11:14:07 -0400

    Refactor: added setup to test

--- test/com/regant/PencilTest.java
+++ test/com/regant/PencilTest.java
@@ -1,15 +1,22 @@
 package com.regant;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PencilTest {
+    Pencil pencil;
+    String paper;
+
+    @BeforeEach
+    void setup(){
+        pencil = new Pencil();
+        paper = "";
+    }
 
     @Test
     void aPencilCanWriteTextOnPaper (){
-        Pencil pencil = new Pencil();
-        String paper = "";
         paper = pencil.write(paper, "Test");
         assertEquals("Test", paper);
     }
@@ -17,8 +24,6 @@ class PencilTest {
     @Test
     void aPencilCanWriteLongerTextOnPaper (){
         String exampleString = "Testing the length of a sentence we can write to paper with a pencil";
-        Pencil pencil = new Pencil();
-        String paper = "";
         paper = pencil.write(paper, exampleString);
         assertEquals(exampleString, paper);
     }
